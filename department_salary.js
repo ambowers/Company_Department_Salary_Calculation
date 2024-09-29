c1bf78ce47d1847c09a1177404d4091a2efd58a3
lina <ambowers@usf.edu>
2024-09-29 13:54:05 -0400

    Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -52,3 +52,35 @@ const company = {
         }
     ]
 };
+
+//Task 2 create a recursive function to calculate total salary for a department
+function calculateDepartmentSalary (department){
+    let totalSalary = 0;
+
+    for (let employee of department.employees){
+        totalSalary += employee.salary;
+
+        if (employee.subordinates && employee.subordinates.length > 0){ //if employee has subordinates, recursively add salaries to total
+            totalSalary += calculateSubordinateSalary(employee);
+        }
+    }
+return totalSalary;
+}
+function calculateSubordinateSalary(employee){
+    let totalSalary = 0;
+
+    for (let subordinate of employee.subordinates) {
+        totalSalary += subordinate.salary;
+
+        if (subordinate.subordinates && subordinate.subordinates.length > 0) {
+            totalSalary += calculateSubordinateSalary(subordinate);
+        }
+    }
+return totalSalary;
+}
+
+const engineerSalary = calculateDepartmentSalary(company.departments[0])// calls row 1(engineering department), but use a zero bc "row 1"="0"
+const marketingSalary = calculateDepartmentSalary(company.departments[1])
+
+console.log(`Total salary for Engineering Department (including subordinates): $${engineerSalary}`);
+console.log(`Total salary for Marketing Department (including subordinates): $${marketingSalary}`);
